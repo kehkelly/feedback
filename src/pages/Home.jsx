deba32c120be392eaa8864db0aab63f1bdaaef27
Kelly Dantas <kdsl.dantas2017@gmail.com>
2024-06-11 04:48:50 +0000

    feet: adição da verificação da nota e do envio da nota para mudar de página

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ButtonContainer, CardContainer, IconContainer, ResultContainer } from './Home.styles.js'
 
 import iconStarImg from "../assets/icon-star.svg"
 import thankYouImg from "../assets/illustration-thank-you.svg"
 
 export function Home() {
-  let aparecerResultado = true
+  const [mostrarResultado, setMostrarResultado] = useState(false)
+  const [feedbackNote, setFeedbackNote] = useState(0)
+
+  function handleFeedbackButtonClick(event) {
+    const feedback = Number(event.target.innerText)
+
+    setFeedbackNote(feedback)
+  }
+
+  function handleSubimit() {
+    if (feedbackNote === 0) return
+
+    setMostrarResultado(true)
+  }
 
   return (
-    aparecerResultado === false ? (
+    mostrarResultado === false ? (
       <CardContainer>
         <IconContainer>
           <img src={iconStarImg} alt="icone de estrela" />
@@ -19,20 +32,20 @@ export function Home() {
         </p>
 
         <ButtonContainer>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
+          <button onClick={handleFeedbackButtonClick}>1</button>
+          <button onClick={handleFeedbackButtonClick}>2</button>
+          <button onClick={handleFeedbackButtonClick}>3</button>
+          <button onClick={handleFeedbackButtonClick}>4</button>
+          <button onClick={handleFeedbackButtonClick}>5</button>
         </ButtonContainer>
 
-        <button>Enviar</button>
+        <button onClick={handleSubimit}>Enviar</button>
     </CardContainer>
     ) : (
       <CardContainer>
         <img src={thankYouImg} alt="imagem de agradecimento" />
         <ResultContainer>
-          <p>Você selecionou 4 de 5</p>
+          <p>Você selecionou {feedbackNote} de 5</p>
         </ResultContainer>
         <h1>Obrigado!</h1>
         <p>Agradecemos por dedicar um momento para nos avaliar. Se precisar de mais suporte não hesite em entrar em contato!</p>
