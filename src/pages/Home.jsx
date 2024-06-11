df82874eb47cd43646f7eca1cfd2befe2f7495f9
Kelly Dantas <kdsl.dantas2017@gmail.com>
2024-06-11 04:34:20 +0000

    style: estilização completa

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,42 @@
 import React from 'react'
-import { CardContainer, IconContainer } from './Home.styles'
+import { ButtonContainer, CardContainer, IconContainer, ResultContainer } from './Home.styles.js'
 
 import iconStarImg from "../assets/icon-star.svg"
+import thankYouImg from "../assets/illustration-thank-you.svg"
 
 export function Home() {
+  let aparecerResultado = true
+
   return (
-    <CardContainer>
-      <IconContainer>
-        <img src={iconStarImg} alt="icone de estrela" />
-      </IconContainer>
+    aparecerResultado === false ? (
+      <CardContainer>
+        <IconContainer>
+          <img src={iconStarImg} alt="icone de estrela" />
+        </IconContainer>
 
-      <h1>Como foi o atendimento?</h1>
-      <p>Conte-nos como foi sua ajuda com sua solicitação. Agradecemos muito seu feedback para podermos melhorar nosso atendimento!
-      </p>
+        <h1>Como foi o atendimento?</h1>
+        <p>Conte-nos como foi sua ajuda com sua solicitação. Agradecemos muito seu feedback para podermos melhorar nosso atendimento!
+        </p>
 
-      <buttonContainer>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>4</button>
-        <button>5</button>
-      </buttonContainer>
-      
-      <button>Enviar</button>
+        <ButtonContainer>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>4</button>
+          <button>5</button>
+        </ButtonContainer>
+
+        <button>Enviar</button>
     </CardContainer>
+    ) : (
+      <CardContainer>
+        <img src={thankYouImg} alt="imagem de agradecimento" />
+        <ResultContainer>
+          <p>Você selecionou 4 de 5</p>
+        </ResultContainer>
+        <h1>Obrigado!</h1>
+        <p>Agradecemos por dedicar um momento para nos avaliar. Se precisar de mais suporte não hesite em entrar em contato!</p>
+      </CardContainer>
+    )
   )
 }
